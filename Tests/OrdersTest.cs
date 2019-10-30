dd4434f701a09829d2afcfa3ed60756ff267d880
Thiago Canete <thiago.canete@gmail.com>
2019-10-30 19:56:24 -0300

    adicao de testes de unidade

--- Tests/OrdersTest.cs
+++ Tests/OrdersTest.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Net;
+using ABE_Atacadista.Controllers;
+using ABE_Atacadista.Models;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace ABE_Atacadista.Tests
@@ -5,13 +10,82 @@ namespace ABE_Atacadista.Tests
     public class OrdersTest
     {
         [Fact]
-        public void TestName()
+        public void GetOrderStatus()
         {
-            //Given
+            // Arrange
+            ResetOrderList();
+            var controller = new OrdersController();
 
-            //When
+            // Act
+            var response = controller.GetOrderStatus(1).Value as OrderStatusResponseDTO;
 
-            //Then
+            // Assert
+            Assert.NotNull(response);
+        }
+
+        [Fact]
+        public void RequestOrder()
+        {
+            // Arrange
+            var controller = new OrdersController();
+            var request = new OrderRequestDTO
+            {
+                Orders = new System.Collections.Generic.List<Order>()
+                {
+                    new Order
+                    {
+                        Code = 2,
+                        Quantity = 12,
+                        Notes = "qaad ads asd"
+                    }
+                }
+            };
+            OrdersController.Orders = new System.Collections.Generic.List<OrderResponseDTO>();
+
+            // Act
+            var response = controller.RequestOrder(request).Value as OrderResponseDTO;
+            var expected = OrderStatus.Created;
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(expected, response.Status);
+        }
+
+        [Fact]
+        public void RespondOrder()
+        {
+            // Arrange
+            var controller = new OrdersController();
+            var request = new OrderAcceptanceResponse();
+
+            // Act
+            var response = controller.RespondOrder(request);
+
+            // Assert
+            Assert.NotNull(response);
+        }
+
+
+        private void ResetOrderList()
+        {
+            OrdersController.Orders = new System.Collections.Generic.List<OrderResponseDTO>()
+            {
+                new OrderResponseDTO
+                {
+                    Id = 1,
+                    DeliveryDate = DateTime.Now,
+                    Orders = new System.Collections.Generic.List<Order>(){
+                        new Order{
+                            Code = 1,
+                            Quantity = 12,
+                            Notes = "testes teste"
+                        }
+                    },
+                    OrderValue = 12.56m,
+                    Status = OrderStatus.Created,
+                    Links = new System.Collections.Generic.List<BaseDTOLink>()
+                }
+            };
         }
     }
 }
